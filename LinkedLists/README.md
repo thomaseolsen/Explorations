0e3dd47179ed631a5aad41eb0972be49ee8c91e4
Thomas Erik Olsen <tolsen@southern.edu>
2018-09-11 18:45:00 -0400

    Completed Linked List C++

--- LinkedLists/README.md
+++ LinkedLists/README.md
@@ -3,6 +3,9 @@
 ## Ada
 
 ## C++
+This is my base implementation of the Doubly Linked List (DLL). From here, I will translate the idea across the various languages and output the results of traversing the list.
+
+For comparison, I also tested std::list available in the C++ Standard Library. This provides similar functionality built-in. I'll compare the implementation of this library to the other languages as well (where available). This is also an implementation of a DLL, and as such offers fairly equivalent functionality to our jumping off point.
 
 ## C#
 
--- LinkedLists/cppSrc/main.cpp
+++ LinkedLists/cppSrc/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <list>
 #include "list.h"
 
 void intList() {
@@ -36,6 +38,21 @@ void intList() {
     free(c);
 }
 
+void intStdList() {
+    std::list<int> linkedList = {1, 2, 3};
+
+    std::cout << "Traversing std::list forward..." << std::endl;
+    for (std::list<int>::const_iterator iter = linkedList.begin(); iter != linkedList.end(); ++iter) {
+        std::cout << "Current Node Value: " << *iter << std::endl;
+    }
+
+    std::cout << "Traversing std::list backward..." << std::endl;
+    for (std::list<int>::const_iterator iter = linkedList.end(); iter != linkedList.begin(); --iter) {
+        if (iter != linkedList.end()) std::cout << "Current Node Value: " << *iter << std::endl;
+    }
+    std::cout << "Current Node Value: " << *linkedList.begin() << std::endl;
+}
+
 void stringList() {
     List<std::string> *linkedList = new List<std::string>();
     struct Node<std::string> *a = (struct Node<std::string>*)malloc(sizeof(struct Node<std::string>));
@@ -70,9 +87,27 @@ void stringList() {
     free(c);
 }
 
+void stringStdList() {
+    std::list<std::string> linkedList = {"One", "Two", "Three"};
+
+    std::cout << "Traversing std::list forward..." << std::endl;
+    for (std::list<std::string>::const_iterator iter = linkedList.begin(); iter != linkedList.end(); ++iter) {
+        std::cout << "Current Node Value: " << *iter << std::endl;
+    }
+
+    std::cout << "Traversing std::list backward..." << std::endl;
+    for (std::list<std::string>::const_iterator iter = linkedList.end(); iter != linkedList.begin(); --iter) {
+        if (iter != linkedList.end()) std::cout << "Current Node Value: " << *iter << std::endl;
+    }
+    std::cout << "Current Node Value: " << *linkedList.begin() << std::endl;
+}
+
 int main(int argc, char *argv[]) {
     intList();
     stringList();
+
+    intStdList();
+    stringStdList();
 
     return 0;
 }
