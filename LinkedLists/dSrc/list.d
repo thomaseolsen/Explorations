f27f504c774dde71a83fda9236e30a4bc282f9ca
Thomas Erik Olsen <tolsen@southern.edu>
2018-09-13 15:36:06 -0400

    Completed Linked List in D

--- LinkedLists/dSrc/list.d
+++ LinkedLists/dSrc/list.d
@@ -1,26 +1,165 @@
 import std.stdio;
 
-// struct Node(T) {
-//     public:
-//         T data;
-//         Node next;
-//         Node prev;
-// }
+class Node(T) {
+    public:
+        T data;
+        Node!(T) next;
+        Node!(T) prev;
+}
 
 class List(T) {
     private:
-        int length;
-        // Node head;
-        // Node tail;
-        // Node curr;
+        int length = 0;
+        Node!(T) head = null;
+        Node!(T) tail = null;
+        Node!(T) curr = null;
 
     public:
         int Length() {
-            return 0;
-        };
+            return length;
+        }
 
-        void foo() {
-            writeln("bar");
+        // Node Access
+        Node!(T) NextNode() {
+            return curr.next;
+        }
+
+        Node!(T) PrevNode() {
+            return curr.prev;
+        }
+
+        // Node Actions
+        /*  This method inserts a node after the current position in the list.  */
+        void InsertAfter(Node!(T) node) {
+            if (length == 0) {
+                // Insert first node in the list
+                head = node;
+                tail = node;
+                curr = node;
+            } else {
+                // Insert a new node in an existing list
+                // Set the new links moving forward (curr -> node -> next)
+                node.next = curr.next;
+                curr.next = node;
+
+                // Set the new links movign backward (curr <- node <- next)
+                if (node.next !is null) // node !is tail
+                    node.next.prev = node;
+                else
+                    tail = node;
+                node.prev = curr;
+            }
+
+            length++;
+        }
+
+        /*  This method inserts a node before the current position in the list.  */
+        void InsertBefore(Node!(T) node) {
+            if (length == 0) {
+                // Insert first node in the list
+                head = node;
+                tail = node;
+                curr = node;
+            } else {
+                // Insert a new node in an existing list
+                // Set the new links moving backward (prev <- node <- curr)
+                node.prev = curr.prev;
+                curr.prev = node;
+
+                // Set the new links movign forward (prev -> node -> curr)
+                if (node.prev !is null) // node !is head
+                    node.prev.next = node;
+                else
+                    head = node;
+                node.next = curr;
+            }
+
+            length++;
+        }
+
+        /*  This method deletes the next node from the list and returns the deleted node.
+            If the next node is NULL (aka: curr = tail) do nothing and return NULL.  */
+        Node!(T) DeleteAfter() {
+            // Get the node we are about to delete.
+            auto tmpNode = curr.next;
+
+            if (length > 0) // Don't deprecate if in an empty list
+                length--;
+
+            // Per our spec, if tmpNode is NULL, we are at the tail and will return NULL.
+            if (tmpNode is null) return null;
+
+            // Remove the link to tmpNode moving forward
+            curr.next = tmpNode.next;
+            // Remove the link to tmpNode moving backward
+            if (tmpNode.next !is null) // tmpNode !is tail
+                tmpNode.next.prev = curr;
+
+            return tmpNode;
+        }
+
+        /*  This method deletes the prev node from the list and returns the deleted node.
+            If the prev node is NULL (aka: curr = head) do nothing and return NULL.  */
+        Node!(T) DeleteBefore() {
+            // Get the node we are about to delete.
+            auto tmpNode = curr.next;
+
+            if (length > 0) // Don't deprecate if in an empty list
+                length--;
+
+            // Per our spec, if tmpNode is NULL, we are at the head and will return NULL.
+            if (tmpNode is null) return null;
+
+            // Remove the link to tmpNode moving backward
+            curr.prev = tmpNode.prev;
+            // Remove the link to tmpNode moving forward
+            if (tmpNode.prev !is null) // tmpNode !is head
+                tmpNode.prev.next = curr;
+
+            return tmpNode;
+        }
+
+        /*  This method moves forward in the list.
+            If the next node is NULL, we are at the tail and don't move forward.  */
+        void StepNext() {
+            if (curr.next !is null) curr = curr.next;
+        }
+
+        /*  This method moves forward in the list.
+            If the prev node is NULL, we are at the head and don't move backward.  */
+        void StepPrev() {
+            if (curr.prev !is null) curr = curr.prev;
+        }
+
+        /*  This method moves to the head of the list.  */
+        void StepHead() {
+            curr = head;
+        }
+
+        /*  This method moves to the tail of the list.  */
+        void StepTail() {
+            curr = tail;
+        }
+
+        // Expose List Data
+        /*  This method returns true if curr = head and false otherwise.  */
+        bool IsHead() {
+            if (curr.prev is null)
+                return true;
+            else
+                return false;
+        }
+
+        /*  This method returns true if curr = tail and false otherwise.  */
+        bool IsTail() {
+            if (curr.next is null)
+                return true;
+            else
+                return false;
+        }
+
+        /*  This method returns curr.data.  */
+        T Data() {
+            return curr.data;
         }
 }
-
--- LinkedLists/dSrc/main.d
+++ LinkedLists/dSrc/main.d
@@ -1,8 +1,67 @@
 import std.stdio;
 import list;
 
-void main(string[] args) {
-    auto a = new List!(int);
+void intList() {
+    auto linkedList = new List!(int);
+    auto a = new Node!(int);
+    auto b = new Node!(int);
+    auto c = new Node!(int);
 
-    a.foo();
+    a.data = 2;
+    b.data = 1;
+    c.data = 3;
+
+    linkedList.InsertAfter(a);
+    linkedList.InsertBefore(b);
+    linkedList.StepNext(); // curr = a
+    linkedList.InsertAfter(c);
+
+    linkedList.StepHead();
+    writeln("Trafersing forward...");
+    for (int i = 0; i < linkedList.Length(); i++) {
+        writeln("Current Node Value: ", linkedList.Data());
+        linkedList.StepNext();
+    }
+
+    linkedList.StepTail();
+    writeln("Trafersing backward...");
+    for (int i = 0; i < linkedList.Length(); i++) {
+        writeln("Current Node Value: ", linkedList.Data());
+        linkedList.StepPrev();
+    }
+}
+
+void stringList() {
+    auto linkedList = new List!(string);
+    auto a = new Node!(string);
+    auto b = new Node!(string);
+    auto c = new Node!(string);
+
+    a.data = "Two";
+    b.data = "One";
+    c.data = "Three";
+
+    linkedList.InsertAfter(a);
+    linkedList.InsertBefore(b);
+    linkedList.StepNext(); // curr = a
+    linkedList.InsertAfter(c);
+
+    linkedList.StepHead();
+    writeln("Trafersing forward...");
+    for (int i = 0; i < linkedList.Length(); i++) {
+        writeln("Current Node Value: ", linkedList.Data());
+        linkedList.StepNext();
+    }
+
+    linkedList.StepTail();
+    writeln("Trafersing backward...");
+    for (int i = 0; i < linkedList.Length(); i++) {
+        writeln("Current Node Value: ", linkedList.Data());
+        linkedList.StepPrev();
+    }
+}
+
+void main(string[] args) {
+    intList();
+    stringList();
 }
