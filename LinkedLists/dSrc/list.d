f3f545ace8f609a775d01732435a57c27dbb8fab
Thomas Erik Olsen <tolsen@southern.edu>
2018-09-12 22:09:44 -0400

    Started framework for Linked List in D

---
+++ LinkedLists/dSrc/list.d
@@ -0,0 +1,26 @@
+import std.stdio;
+
+// struct Node(T) {
+//     public:
+//         T data;
+//         Node next;
+//         Node prev;
+// }
+
+class List(T) {
+    private:
+        int length;
+        // Node head;
+        // Node tail;
+        // Node curr;
+
+    public:
+        int Length() {
+            return 0;
+        };
+
+        void foo() {
+            writeln("bar");
+        }
+}
+
---
+++ LinkedLists/dSrc/main.d
@@ -0,0 +1,8 @@
+import std.stdio;
+import list;
+
+void main(string[] args) {
+    auto a = new List!(int);
+
+    a.foo();
+}
