a29c3c3c967b6c6b840c75a9a262839247d7da3a
Thomas Erik Olsen <tolsen@southern.edu>
2018-09-28 22:28:20 -0400

    Linked List in C# compiles, just a few tweaks left

--- LinkedLists/csSrc/list.cs
+++ LinkedLists/csSrc/list.cs
@@ -1,8 +1,12 @@
 namespace LList {
     class Node<T> {
-        T data;
-        Node<T> next;
-        Node<T> prev;
+        public T data;
+        public Node<T> next;
+        public Node<T> prev;
+
+        public Node(T val) {
+            data = val;
+        }
     }
 
     class List<T> {
@@ -11,7 +15,7 @@ namespace LList {
         private Node<T> tail;
         private Node<T> curr;
 
-        public void List<T>() {
+        public List() {
             length = 0;
             head = null;
             tail = null;
@@ -35,24 +39,92 @@ namespace LList {
 #region "Node Actions"
         /*  This method inserts a node after the current position in the list.  */
         public void InsertAfter(Node<T> node) {
-            //do something
+            if (length == 0) {
+                // Insert first node in the list
+                head = node;
+                tail = node;
+                curr = node;
+            } else {
+                // Insert a new node in an existing list
+                // Set the new links moving forward (curr -> node -> next)
+                node.next = curr.next;
+                curr.next = node;
+
+                // Set the new links moving backward (curr <- node <- next)
+                if (node.next != null) // node != tail
+                    node.next.prev = node;
+                else
+                    tail = node;
+                node.prev = curr;
+            }
+
+            length++;
         }
 
         /*  This method inserts a node before the current position in the list.   */
         public void InsertBefore(Node<T> node) {
-            //do something
+            if (length == 0) {
+                // Insert first node in the list
+                head = node;
+                tail = node;
+                curr = node;
+            } else {
+                // Insert a new node in an existing list
+                // Set the new links moving backward (prev <- node <- curr)
+                node.prev = curr.prev;
+                curr.prev = node;
+
+                // Set the new links moving forward (prev -> node -> curr)
+                if (node.prev != null) // node != head
+                    node.prev.next = node;
+                else
+                    head = node;
+                node.next = curr;
+            }
+
+            length++;
         }
 
         /*  This method deletes the next node from the list and returns the deleted node.
             If the next node is null (aka: curr = tail) do nothing and return null.  */
         public Node<T> DeleteAfter() {
-            //do something
+            // Get the node we are about to delete.
+            Node<T> tmpNode = curr.next;
+
+            if (length > 0) // Don't deprecate if in an empty list
+                length--;
+
+            // Per our spec, if tmpNode is null, we are at the tail and will return null.
+            if (tmpNode == null) return null;
+
+            // Remove the link to tmpNode moving forward
+            curr.next = tmpNode.next;
+            // Remove the link to tmpNode moving backward
+            if (tmpNode.next != null) // tmpNode != tail
+                tmpNode.next.prev = curr;
+
+            return tmpNode;
         }
 
         /*  This method deletes the prev node from the list and returns the deleted node.
             If the prev node is null (aka: curr = head) do nothing and return null.  */
         public Node<T> DeleteBefore() {
-            //do something
+            // Get the node we are about to delete.
+            Node<T> tmpNode = curr.next;
+
+            if (length > 0) // Don't deprecate if in an empty list
+                length--;
+
+            // Per our spec, if tmpNode is null, we are at the head and will return null.
+            if (tmpNode == null) return null;
+
+            // Remove the link to tmpNode moving backward
+            curr.prev = tmpNode.prev;
+            // Remove the link to tmpNode moving forward
+            if (tmpNode.prev != null) // tmpNode != head
+                tmpNode.prev.next = curr;
+
+            return tmpNode;
         }
 
         /*  This method moves forward in the list.
--- LinkedLists/csSrc/main.cs
+++ LinkedLists/csSrc/main.cs
@@ -3,8 +3,60 @@ using LList;
 namespace Main {
     class Program {
         static void Main(string[] args) {
-            var list = new LinkedList<int>();
-            System.Console.WriteLine(list.length());
+            intList();
+            stringList();
+        }
+
+        static void intList() {
+            List<int> linkedList = new List<int>();
+            Node<int> a = new Node<int>(2);
+            Node<int> b = new Node<int>(1);
+            Node<int> c = new Node<int>(3);
+
+            linkedList.InsertAfter(a);
+            linkedList.InsertBefore(b);
+            linkedList.StepNext(); // curr = a
+            linkedList.InsertAfter(c);
+
+            linkedList.StepHead();
+            System.Console.WriteLine("Trafersing forward...");
+            for (int i = 0; i < linkedList.Length(); i++) {
+                System.Console.WriteLine("Current Node Value: ", linkedList.Data());
+                linkedList.StepNext();
+            }
+
+            linkedList.StepTail();
+            System.Console.WriteLine("Trafersing backward...");
+            for (int i = 0; i < linkedList.Length(); i++) {
+                System.Console.WriteLine("Current Node Value: ", linkedList.Data());
+                linkedList.StepPrev();
+            }
+        }
+
+        static void stringList() {
+            List<string> linkedList = new List<string>();
+            Node<string> a = new Node<string>("Two");
+            Node<string> b = new Node<string>("One");
+            Node<string> c = new Node<string>("Three");
+
+            linkedList.InsertAfter(a);
+            linkedList.InsertBefore(b);
+            linkedList.StepNext(); // curr = a
+            linkedList.InsertAfter(c);
+
+            linkedList.StepHead();
+            System.Console.WriteLine("Trafersing forward...");
+            for (int i = 0; i < linkedList.Length(); i++) {
+                System.Console.WriteLine("Current Node Value: ", linkedList.Data());
+                linkedList.StepNext();
+            }
+
+            linkedList.StepTail();
+            System.Console.WriteLine("Trafersing backward...");
+            for (int i = 0; i < linkedList.Length(); i++) {
+                System.Console.WriteLine("Current Node Value: ", linkedList.Data());
+                linkedList.StepPrev();
+            }
         }
     }
 }
